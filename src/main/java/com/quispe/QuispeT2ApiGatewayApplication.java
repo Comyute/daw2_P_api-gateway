b17cfe943df816c947715bf12cb1a59b481ce7bb
Comyute <alexiscalderon1599@gmail.com>
2025-06-22 23:31:58 -0500

    first commit

---
+++ src/main/java/com/quispe/QuispeT2ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.quispe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuispeT2ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuispeT2ApiGatewayApplication.class, args);
+	}
+
+}
