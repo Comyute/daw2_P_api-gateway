9e9e8ec3ad3f7ad41aecabbec9b22b8c9d493022
Comyute <alexiscalderon1599@gmail.com>
2025-07-03 00:40:33 -0500

    group modificado

--- src/main/java/com/dibujitos/ApiGatewayApplication.java
+++ src/main/java/com/dibujitos/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.quispe;
+package com.dibujitos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/dibujitos/ApiGatewayApplicationTests.java
+++ src/test/java/com/dibujitos/ApiGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.quispe;
+package com.dibujitos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
